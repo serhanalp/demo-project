0786ae5cdc123ead15daf8e626bdded9793900ff
serhanalp <serhanalpaydin@gmail.com>
2023-03-04 08:05:58 -0600

    Fixed IP and changed browser type to remote-chrome

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "54.160.113.31";
+                        String gridAddress = "18.212.201.17";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
